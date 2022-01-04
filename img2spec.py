f70acaa86d2aead5569eebaaf984b792fc1b1eed
2ntense <stefanyip@hotmail.com>
2022-01-04 11:52:23 +0100

    Set the duration of signal and position of image

--- img2spec.py
+++ img2spec.py
@@ -1,29 +1,29 @@
 import struct
+import wave
 
-from PIL import Image
+import matplotlib.pyplot as plt
 import numpy
 import scipy.ndimage
 import scipy.signal
-import wave
-import matplotlib.pyplot as plt
+from PIL import Image
+
+
+def show_img(arr):
+    pil_image = Image.fromarray(arr)
+    pil_image.show()
 
 
 def load_image(img_path, freq_step, samp_rate, duration):
     with Image.open(img_path) as img:
-        # convert to grayscale
         img = img.convert("L")
         img_data = numpy.array(img)
-        # flip along the horizontal axis
-        img_data = numpy.flip(img_data, axis=0)
         max_sample = duration * samp_rate
-        # determine zoom factor
         zoom_factor = ((samp_rate / 2) / freq_step / img_data.shape[0], max_sample / img_data.shape[1])
-        # apply zoom on image array
         img_data = scipy.ndimage.zoom(img_data, zoom_factor, order=0)
         return img_data
 
 
-def max_min_norm(data, upper, lower):
+def rescale(data, upper, lower):
     return [
         lower + (element - data.min()) * (upper - lower) / (data.max() - data.min())
         for
@@ -31,6 +31,10 @@ def max_min_norm(data, upper, lower):
         in
         data
     ]
+
+
+def rescale_val(val, scaled_max, scaled_min, max_val, min_val):
+    return scaled_min + (val - min_val) * (scaled_max - scaled_min) / (max_val - min_val)
 
 
 def show_spectrogram(data, rate):
@@ -42,13 +46,21 @@ def show_spectrogram(data, rate):
     plt.show()
 
 
-def write_to_wav(out_file, data, rate):
+def write_to_wav(out_file, data, rate, lpad=0, rpad=0):
+    zero_byte = struct.pack("h", 0)
+
     with wave.open(out_file, "w") as wav_file:
         wav_file.setparams((1, 2, rate, 0, 'NONE', 'not compressed'))
+
+        for _ in range(int(lpad)):
+            wav_file.writeframes(zero_byte)
+
         for i, sample in enumerate(data):
-            print(f"writing sample {i + 1}/{len(data)}")
             to_write = struct.pack("h", int(sample * 32767.0))
             wav_file.writeframes(to_write)
+
+        for _ in range(int(rpad)):
+            wav_file.writeframes(zero_byte)
 
 
 def gen_wave(freq, amp, samp_step, samp_rate):
@@ -58,8 +70,20 @@ def gen_wave(freq, amp, samp_step, samp_rate):
     return amp * numpy.sin(periods * 2 * numpy.pi * samples / samp_step)
 
 
-def img2spec(img_path, out_path="img2spec.wav", freq_step=175, samp_step=1050, samp_rate=22050, duration=5.0):
-    img_data = load_image(img_path, freq_step=freq_step, samp_rate=samp_rate, duration=duration)
+def img2spec(
+        img_path,
+        out_path,
+        duration,
+        img_freq_interval,  # tuple
+        img_time_interval,  # tuple
+        samp_rate,
+        freq_step=175,
+        samp_step=1050,
+):
+    img_duration = img_time_interval[1] - img_time_interval[0]
+    img_data = load_image(img_path, freq_step=freq_step, samp_rate=samp_rate,
+                          duration=img_duration)
+    img_data = numpy.flip(img_data, axis=0)
     img_data = numpy.transpose(img_data)
 
     out_signal = numpy.array([])
@@ -73,13 +97,18 @@ def img2spec(img_path, out_path="img2spec.wav", freq_step=175, samp_step=1050, s
         for y in range(img_data.shape[1] - 1, 0, -1):
             if img_data[x][y] < 255:
                 amp = 1 - img_data[x][y] / 255
-                signal += gen_wave(y * freq_step, amp, samp_step, samp_rate)
+                rescaled_freq = rescale_val(freq_step * y, img_freq_interval[1], img_freq_interval[0],
+                                       freq_step * (img_data.shape[1] - 1), 0)
+                signal += gen_wave(rescaled_freq, amp, samp_step, samp_rate)
         out_signal = numpy.append(out_signal, signal)
 
-    normalized = max_min_norm(out_signal, 1, -1)
-    # show_spectrogram(normalized, samp_rate)
-    # show_spectrogram(out_signal, samp_rate)
-    write_to_wav(out_path, normalized, samp_rate)
+    rescaled_out_signal = rescale(out_signal, 1, -1)
+
+    lpad = img_time_interval[0] * samp_rate
+    rpad = (duration - img_time_interval[1]) * samp_rate
+
+    write_to_wav(out_path, rescaled_out_signal, samp_rate, lpad=lpad, rpad=rpad)
 
 
-img2spec(img_path="gradient2.png", out_path="img2spec.wav", freq_step=25, samp_step=1050, samp_rate=22050)
+img2spec(img_path="smiley_double.png", out_path="img2spec.wav", samp_rate=48000, duration=8.0,
+         img_freq_interval=(8000, 10000), img_time_interval=(3.0, 5.0))
